283d31c9325cdc6cc5d044d89c9b417c4ec3bd12
Jan Dockal <jan.dockal@metservice.com>
2021-07-21 08:56:27 +1200

    Improve typings for MapboxLayer

--- src/deckgl-mapbox.d.ts
+++ src/deckgl-mapbox.d.ts
@@ -7,8 +7,8 @@ declare module '@deck.gl/mapbox' {
 
 	// I wish I could do something like `declare class MapboxLayer extends CustomLayerInterface {` :sadface:
 	// https://stackoverflow.com/a/23225358
-	declare class MapboxLayer {
-		constructor(props: Props): void;
+	declare class MapboxLayer<P extends Props> {
+		constructor(props: P): void;
 		id: CustomLayerInterface['id'];
 		onAdd: CustomLayerInterface['onAdd'];
 		onRemove: CustomLayerInterface['onRemove'];
@@ -16,6 +16,7 @@ declare module '@deck.gl/mapbox' {
 		render: CustomLayerInterface['render'];
 		renderingMode: CustomLayerInterface['renderingMode'];
 		type: CustomLayerInterface['type'];
+		setProps(props: P): void;
 	}
 
 	export { MapboxLayer };

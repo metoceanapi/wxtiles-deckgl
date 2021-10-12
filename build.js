f610c850d4e05ba5eca83db3a2d120bb01ed69de
SergeiMelman <s.melman@metocean.co.nz>
2021-10-12 18:41:27 +1300

    build externals for 'iife', not 'esm'

--- build.js
+++ build.js
@@ -10,15 +10,7 @@ const sharedConfig = {
 		'.fs': 'text',
 		'.vs': 'text',
 	},
-	plugins: [
-		externalGlobalPlugin({
-			'@deck.gl/core': 'window.deck',
-			'@deck.gl/layers': 'window.deck',
-			'@deck.gl/geo-layers': 'window.deck',
-			'@luma.gl/core': 'window.luma',
-			'@luma.gl/webgl': 'window.luma',
-		}),
-	],
+	
 	// https://www.stetic.com/market-share/browser/
 	target: ['es2020', 'chrome80', 'safari13', 'edge89', 'firefox70'],
 	minify: true,
@@ -37,6 +29,16 @@ esbuild
 esbuild
 	.build({
 		...sharedConfig,
+		plugins: [
+			externalGlobalPlugin({
+				'@deck.gl/core': 'window.deck',
+				'@deck.gl/layers': 'window.deck',
+				'@deck.gl/geo-layers': 'window.deck',
+				'@luma.gl/core': 'window.luma',
+				'@luma.gl/webgl': 'window.luma',
+			}),
+		],
+
 		format: 'iife',
 		outfile: 'dist/web/wxtilesdeckgl.js',
 		globalName: 'wxtilesdeckgl',

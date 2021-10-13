48342d996e1ec0f8c0bd5951bc6eb1c28548b350
SergeiMelman <s.melman@metocean.co.nz>
2021-10-14 00:27:36 +1300

    +

--- src_example/dev-deckgl-index.ts
+++ src_example/dev-deckgl-index.ts
@@ -19,14 +19,6 @@ import units from './styles/uconv.json';
 import colorSchemes from './styles/colorschemes.json';
 
 async function start() {
-	const deckgl = new Deck({
-		initialViewState: { latitude: -38, longitude: 176, zoom: 4 },
-		controller: true,
-		parent: document.getElementById('map')!,
-		views: [new MapView({ repeat: true })],
-		layers: [],
-	});
-
 	// ESSENTIAL step to get lib ready.
 	const wxlibCustomSettings: LibSetupObject = {
 		colorStyles: colorStyles as any,
@@ -37,15 +29,9 @@ async function start() {
 	setWxTilesLogging(true); // logging on
 
 	const params: WxServerVarsStyleType =
-		//
-		// ['nz_wave_trki', 'hs_mean', 'Significant wave height'];
-		// ['ecwmf.global', 'air.temperature.at-2m', 'temper2m'];
-		['ecwmf.global', 'air.temperature.at-2m', 'Sea Surface Temperature'];
-	// ['ecwmf.global', 'air.humidity.at-2m', 'base'];
-	// ['ww3-ecmwf.global', 'wave.height', 'Significant wave height'];
-	// ['ww3-ecmwf.global', 'wave.direction.above-8s.peak', 'direction'];
-	// ['obs-radar.rain.nzl.national', 'reflectivity', 'rain.EWIS'];
-	// ['ecwmf.global', ['wind.speed.eastward.at-10m', 'wind.speed.northward.at-10m'], 'Wind Speed2'];
+		['obs-radar.rain.nzl.national', 'reflectivity', 'rain.EWIS'];
+		// ['ecwmf.global', ['wind.speed.eastward.at-10m', 'wind.speed.northward.at-10m'], 'Wind Speed2'];
+		// ['ecwmf.global', 'air.temperature.at-2m', 'Sea Surface Temperature'];
 	const extraParams = {
 		// DeckGl layer's common parameters
 		opacity: 0.5,
@@ -55,14 +41,27 @@ async function start() {
 		},
 	};
 
-	const wxProps = await createWxTilesLayerProps({ server: 'https://tiles.metoceanapi.com/data/', params, extraParams } as CreateProps);
+	const wxProps = await createWxTilesLayerProps({ server: 'https://tiles.metoceanapi.com/data/', params, extraParams });
+
+	const deckgl = new Deck({
+		initialViewState: { latitude: -38, longitude: 176, zoom: 4 },
+		controller: true,
+		parent: document.getElementById('map')!,
+		views: [new MapView({ repeat: true })],
+		layers: [],
+	});
 
 	const layerManager = createDeckGlLayer(deckgl, wxProps);
 	// or
 	// const layerManager = new WxTilesLayerManager({ deckgl, props: wxProps });
 
 	await layerManager.renderCurrentTimestep();
+	
+	UIhooks(layerManager);
+	debugLayers(deckgl, wxProps.maxZoom);
+}
 
+function debugLayers(deckgl: Deck, maxZoom?: number) {
 	const debugLayerRed = new DebugTilesLayer({
 		id: 'debugtilesR',
 		data: { color: [255, 0, 0, 120] },
@@ -70,11 +69,13 @@ async function start() {
 	const debugLayerBlue = new DebugTilesLayer({
 		id: 'debugtilesB',
 		data: { color: [0, 0, 255, 120] },
-		maxZoom: wxProps.maxZoom,
+		maxZoom,
 	});
 
 	deckgl.setProps({ layers: [...deckgl.props.layers, debugLayerRed, debugLayerBlue] });
+}
 
+function UIhooks(layerManager: WxTilesLayerManager) {
 	// set up user interface
 	const nextButton = document.getElementById('next');
 	const prevButton = document.getElementById('prev');

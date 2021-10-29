e44d0d77736ed884988037352ebb7f1629b1c37d
SergeiMelman <s.melman@metocean.co.nz>
2021-10-29 15:16:54 +1300

    legend to example

--- src_example/dev-deckgl-index.ts
+++ src_example/dev-deckgl-index.ts
@@ -11,6 +11,8 @@ import {
 	LibSetupObject,
 	WxTilesLayerManager,
 	WxTilesLayer,
+	WxGetColorStyles,
+	createLegend,
 } from '../src/wxtilesdeckgl';
 // } from '@metoceanapi/wxtiles-deckgl';
 import '../wxtilescss.css';
@@ -65,6 +67,15 @@ async function start() {
 
 	UIhooks(layerManager);
 	debugLayers(deckgl, wxProps.maxZoom);
+
+	const legendCanvasEl = document.getElementById('legend') as HTMLCanvasElement;
+	if (!legendCanvasEl) return;
+
+	const style = WxGetColorStyles()[params[2]];
+	const legend = createLegend(legendCanvasEl.width - 50, style);
+
+	const txt = params[2] + ' (' + legend.units + ')';
+	drawLegend({ legend, txt, canvas: legendCanvasEl });
 }
 
 function debugLayers(deckgl: Deck, maxZoom?: number) {
@@ -102,6 +113,76 @@ function UIhooks(layerManager: WxTilesLayerManager) {
 		isPlaying && play();
 		playButton.innerHTML = isPlaying ? 'Stop' : 'Play';
 	});
+}
+
+function drawLegend({ legend, canvas, txt }) {
+	if (!canvas || !legend) return;
+
+	const { width, height } = canvas;
+	const halfHeight = (16 + height) >> 2;
+
+	// draw legend
+	const ctx = canvas.getContext('2d');
+	const imData = ctx.createImageData(width, height);
+	const im = new Uint32Array(imData.data.buffer);
+	im.fill(-1);
+
+	const startX = 2;
+	const startY = 2;
+	const startXY = startX + width * startY;
+
+	const trSize = halfHeight >> 1;
+	// left triangle
+	if (legend.showBelowMin) {
+		const c = legend.colors[0];
+		if (c) {
+			for (let x = 0; x < trSize; ++x) {
+				for (let y = trSize; y < trSize + x; ++y) {
+					im[startXY + x + y * width] = c;
+					im[startXY + x + (trSize * 2 - y) * width] = c;
+				}
+			}
+		}
+	}
+
+	for (let x = 0; x < legend.size; ++x) {
+		for (let y = 0; y < halfHeight; ++y) {
+			if (legend.colors[0]) {
+				im[startX + x + trSize + (y + startY + 1) * width] = legend.colors[x];
+			}
+		}
+	}
+
+	// right triangle
+	if (legend.showAboveMax) {
+		const c = legend.colors[legend.colors.length - 1];
+		if (c) {
+			for (let x = 0; x <= trSize; ++x) {
+				for (let y = trSize; y < trSize + x; ++y) {
+					im[startXY + trSize * 2 + legend.size - x + y * width] = c;
+					im[startXY + trSize * 2 + legend.size - x + (trSize * 2 - y) * width] = c;
+				}
+			}
+		}
+	}
+
+	ctx.putImageData(imData, 0, 0);
+
+	// draw ticks
+	ctx.font = '8px sans-serif';
+	ctx.beginPath();
+	for (const tick of legend.ticks) {
+		ctx.strokeStyle = '#000';
+		ctx.moveTo(tick.pos + trSize + startX + 1, startY + 3);
+		ctx.lineTo(tick.pos + trSize + startX + 1, halfHeight);
+		ctx.fillText(tick.dataString, tick.pos + trSize + startX + 1, halfHeight + 11);
+	}
+	ctx.font = '12px sans-serif';
+	ctx.fillText(txt, 13, height - 5);
+	ctx.stroke();
+
+	ctx.strokeStyle = '#888';
+	ctx.strokeRect(1, 1, width - 3, height - 2); //for white background
 }
 
 start();
